97a958fdf4db516bd2f0c3b29d365045e75875fd
sloli-sonnguyen <sloli.sonnguyen@gmail.com>
2020-07-17 12:37:30 +0700

    edit index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const express = require('express');
 var bodyParser = require('body-parser');
 
@@ -34,6 +34,24 @@ const authRoute = require('./routers/auth.route');
 const authMiddleware = require('./middleware/auth.middleware');
 
 // app use
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use('/api/v1/users', authMiddleware, userRoute);
 app.use('/api/v1/posts', authMiddleware, postRoute);
 app.use('/api/v1/comments', authMiddleware, commentRoute);
